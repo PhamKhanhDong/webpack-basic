5bc0f5e91ea00f3b2ed8f0321e4745be16f6f760
DONGVV <khanhdong95vip@gmail.com>
2019-02-18 10:51:20 +0700

    lear form reactJS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Form from './components/Form';
+import JobManager from './components/JobManager';
 import 'bootstrap3/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './css/style.scss';
 
 ReactDOM.render(
-    <App/>,
+    <JobManager/>,
     document.getElementById("root")
 );
